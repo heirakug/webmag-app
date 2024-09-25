597269e69350c61ffe5f1f1d6433dcae65fac58f
heirakug <heirakug@yahoo.co.jp>
2024-09-25 15:30:09 +0900

    initial project

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// [add admin auth 20240925]
+use App\Http\Controllers\Admin\AdminLoginController;
+use App\Http\Controllers\Admin\AdminRegisterController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+/*
+|--------------------------------------------------------------------------
+| 管理者用ルーティング
+|--------------------------------------------------------------------------
+*/
+Route::group(['prefix' => 'admin'], function () {
+    // 登録
+    Route::get('register', [AdminRegisterController::class, 'create'])
+        ->name('admin.register');
+
+    Route::post('register', [AdminRegisterController::class, 'store']);
+
+    // ログイン
+    Route::get('login', [AdminLoginController::class, 'showLoginPage'])
+        ->name('admin.login');
+
+    Route::post('login', [AdminLoginController::class, 'login']);
+
+    // 以下の中は認証必須のエンドポイントとなる
+    Route::middleware(['auth:admin'])->group(function () {
+        // ダッシュボード
+        Route::get('dashboard', fn() => view('admin.dashboard'))
+            ->name('admin.dashboard');
+    });
+});
